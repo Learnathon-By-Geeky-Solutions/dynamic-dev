9241edccb094f3ed53ba89bdb17d442d18af7a92
rashed <rashedroni3@gmail.com>
2025-01-21 16:07:32 +0600

    added clean architecture and repository pattern

---
+++ project/Easy-Travell-BD/Easy-Travell-BD/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Easy_Travell_BD.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
