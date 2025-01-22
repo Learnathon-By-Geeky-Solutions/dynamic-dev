601e7a96c18ba0320d37213e9fea367aacb0f380
rashed <rashedroni3@gmail.com>
2025-01-22 15:24:39 +0600

    completed bus migration and user migration also database connection

--- project/Easy-Travell-BD/Easy-Travell-BD/Program.cs
+++ project/Easy-Travell-BD/Easy-Travell-BD/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+using EasyTravel.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
--- project/Easy-Travell-BD/EasyTravel.Infrastructure/Repositories/BusRepository.cs
+++ project/Easy-Travell-BD/EasyTravel.Infrastructure/Repositories/BusRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EasyTravel.Domain.Interfaces;
+using EasyTravel.Domain.Models;
+using EasyTravel.Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +9,37 @@ using System.Threading.Tasks;
 
 namespace EasyTravel.Infrastructure.Repositories
 {
-    public class BusRepository
+    public class BusRepository : IBusRepository
     {
 
+        private readonly ApplicationDbContext _context;
+
+        public BusRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
 
 
+        public void Addbus(Bus bus)
+        {
+            _context.Buses.Add(bus);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Bus> GetAllBuses()
+        {
+            return _context.Buses.ToList();
+        }
+
+        public Bus GetBusbyId(int busServiceId)
+        {
+            var bus = _context.Buses.Find(busServiceId);
+            if (bus == null)
+            {
+                throw new KeyNotFoundException($"Bus with ID {busServiceId} was not found.");
+            }
+            return bus;
+        }
     }
 }
