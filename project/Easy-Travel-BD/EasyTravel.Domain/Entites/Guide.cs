a0b6f514cb614896f27338ac7f7f428b935af12b
IrfanMahmud <irfanmahmud201@gmail.com>
2025-02-04 09:28:52 +0600

    added entities Agency , Photographer and Guide

---
+++ project/Easy-Travel-BD/EasyTravel.Domain/Entites/Guide.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Domain.Entites
+{
+    public class Guide : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+
+        
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        [MaxLength(500)]
+        public string Bio { get; set; }
+
+        [StringLength(500)]
+        public string LanguagesSpoken { get; set; }
+
+        public string Specialization { get; set; }
+
+        public int YearsOfExperience { get; set; }
+
+        public string Certifications { get; set; }
+
+        public string LicenseNumber { get; set; }
+
+        public bool Availability { get; set; }
+
+        public decimal HourlyRate { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        
+        public string Status { get; set; }
+
+        public int AgencyId { get; set; } // Foreign Key
+
+        public Agency Agency { get; set; } // Navigation Property
+
+    }
+
+}
---
+++ project/Easy-Travel-BD/EasyTravel.Domain/Entites/Photographer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Domain.Entites
+{
+    public class Photographer : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+
+        
+        public string FirstName { get; set; }
+
+        
+        public string LastName { get; set; }
+
+        
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        [MaxLength(500)]
+        public string Bio { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        
+        public string Status { get; set; }
+
+        public string SocialMediaLinks { get; set; }
+
+        public string Skills { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string PortfolioUrl { get; set; }
+
+        public int AgencyId { get; set; } // Foreign Key
+        public Agency Agency { get; set; } // Navigation 
+    }
+}
