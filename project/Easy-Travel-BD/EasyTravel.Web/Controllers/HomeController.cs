b7a050bdb994f09229be8f3e6e2117dc5582ebb9
IrfanMahmud <irfanmahmud201@gmail.com>
2025-02-06 00:32:13 +0600

    Added GetUserController method sig in IUserService and implemented in UserService and used dependency injection in Account and Home controller

--- project/Easy-Travel-BD/EasyTravel.Web/Controllers/HomeController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using EasyTravel.Application.Services;
+using EasyTravel.Domain.Services;
 using EasyTravel.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +9,21 @@ namespace EasyTravel.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ISessionService _sessionService;
+        private readonly IUserService _userService;
+        public HomeController(ILogger<HomeController> logger,ISessionService sessionService, IUserService userService)
         {
             _logger = logger;
+            _sessionService = sessionService;
+            _userService = userService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HttpContext.Response.Headers["Cache-Control"]= "no-store, no-cache, must-revalidate";
+            var isLoggedIn = _userService.IsLoggedIn(_sessionService.GetString("UserLoggedIn"));
+            var controller = _userService.GetUserController(_sessionService.GetString("UserRole"));
+            return isLoggedIn == false ? View() : RedirectToAction("Index", controller, new {area = "Admin"});
         }
 
         public IActionResult Privacy()
