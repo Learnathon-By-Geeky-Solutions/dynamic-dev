bf82f1a2685e595440ad4c0e44c29e7bff4db6d3
IrfanMahmud <irfanmahmud201@gmail.com>
2025-02-08 23:47:01 +0600

    created and updated migration with updated Photographer and Guide Entities

--- project/Easy-Travel-BD/EasyTravel.Domain/Entites/Photographer.cs
+++ project/Easy-Travel-BD/EasyTravel.Domain/Entites/Photographer.cs
@@ -11,18 +11,20 @@ namespace EasyTravel.Domain.Entites
     {
         public Guid Id { get; set; }
 
-        
+
+        [StringLength(100)]
         public required string FirstName { get; set; }
 
-        
+
+        [StringLength(100)]
         public required string LastName { get; set; }
 
-        
+
         [EmailAddress]
         public required string Email { get; set; }
 
         [Phone]
-        public required string PhoneNumber { get; set; }
+        public required string ContactNumber { get; set; }
 
         public required string Address { get; set; }
 
@@ -34,20 +36,28 @@ namespace EasyTravel.Domain.Entites
         [DataType(DataType.Date)]
         public required DateTime DateOfBirth { get; set; }
 
-        public DateTime HireDate { get; set; }
-
-        public DateTime UpdatedAt { get; set; }
-        public string Status { get; set; } = "Active";
-
-        public string? SocialMediaLinks { get; set; }
-
         public required string Skills { get; set; }
+        public required string PortfolioUrl { get; set; }
+
+        public required string Specialization { get; set; }
+
+        public required int YearsOfExperience { get; set; }
+
+        public required bool Availability { get; set; }
+
+        public required decimal HourlyRate { get; set; }
 
         public decimal Rating { get; set; }
 
-        public string? PortfolioUrl { get; set; }
+        public DateTime HireDate { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string? SocialMediaLinks { get; set; }
+        public string? Status { get; set; }
 
         public required Guid AgencyId { get; set; } // Foreign Key
-        public Agency Agency { get; set; } // Navigation 
+
+        public Agency Agency { get; set; } // Navigation Property
     }
 }
