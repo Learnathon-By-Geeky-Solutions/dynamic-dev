07df9116c0b8dbe01c764c7cef435955806ff624
IrfanMahmud <irfanmahmud201@gmail.com>
2025-02-14 07:22:04 +0600

    resolved conflicts before merge to develop and modifies the WebModule

--- project/Easy-Travel-BD/EasyTravel.Application/Services/UserService.cs
+++ project/Easy-Travel-BD/EasyTravel.Application/Services/UserService.cs
@@ -13,41 +13,29 @@ namespace EasyTravel.Application.Services
 {
      public class UserService : IUserService
     {
-        private readonly IApplicationUnitOfWork _applicationUnitOfWork;
+
+        private readonly IApplicationUnitOfWork _applicationUnitOfWork1;
         public UserService(IApplicationUnitOfWork applicationUnitOfWork)
         {
-            _applicationUnitOfWork = applicationUnitOfWork;
+            _applicationUnitOfWork1 = applicationUnitOfWork;
+
         }
+
+
         public bool AuthenticateUser(string email, string password)
         {
-            return _applicationUnitOfWork.UserRepository.ValidateUser(email, password);
+            return _applicationUnitOfWork1.UserRepository.ValidateUser(email, password);
         }
 
         public User GetUserByEmail(string email)
         {
-            return _applicationUnitOfWork.UserRepository.GetUserByEmail(email);
-        }
-
-        public string GetUserController(string role)
-        {
-            return role == "Admin" 
-                ? "Dashboard" 
-                : "Home";
-        }
-
-        public bool IsAdmin(string role)
-        {
-            return role == "Admin";
-        }
-
-        public bool IsLoggedIn(string status)
-        {
-            return status == "true";
+            return _applicationUnitOfWork1.UserRepository.GetUserByEmail(email);
         }
 
         public void RegisterUser(User user)
         {
-            _applicationUnitOfWork.UserRepository.AddUser(user);
+            _applicationUnitOfWork1.UserRepository.AddUser(user);
+            _applicationUnitOfWork1.Save();
         }
     }
 }
