2c15851c51f24c8d5786a857a1f13a880f4d089a
IrfanMahmud <irfanmahmud201@gmail.com>
2025-02-14 09:08:02 +0600

    added and implemented controller with get and post metheods for CRUD and factory, for guide entity

---
+++ project/Easy-Travel-BD/EasyTravel.Application/Factories/GuideFactory.cs
@@ -0,0 +1,37 @@
+﻿using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Application.Factories
+{
+    public class GuideFactory : IEntityFactory<Guide>
+    {
+        public Guide CreateInstance()
+        {
+            return new Guide
+            {
+                FirstName = string.Empty,
+                LastName = string.Empty,
+                Email = string.Empty,
+                ContactNumber = string.Empty,
+                Address = string.Empty,
+                ProfilePicture = string.Empty,
+                Bio = string.Empty,
+                DateOfBirth = DateTime.MinValue,
+                LanguagesSpoken = string.Empty,
+                LicenseNumber = string.Empty,
+                Specialization = string.Empty,
+                YearsOfExperience = 0,
+                Availability = false,
+                HourlyRate = 0,
+                Rating = 0,
+                Status = null,
+                AgencyId = default
+            };
+        }
+    }
+}
--- project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/GuideController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/GuideController.cs
@@ -1,4 +1,7 @@
-﻿using EasyTravel.Domain.Entites;
+﻿using EasyTravel.Application.Factories;
+using EasyTravel.Application.Services;
+using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Factories;
 using EasyTravel.Domain.Services;
 using EasyTravel.Web.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +11,82 @@ namespace EasyTravel.Web.Areas.Admin.Controllers
     [Area("Admin")]
     public class GuideController : Controller
     {
-        
+        private readonly IGuideService _guideService;
+        private readonly IAgencyService _agencyService;
+        private readonly IEntityFactory<Guide> _guideFactory;
+        public GuideController(IGuideService guideService, IAgencyService agencyService,IEntityFactory<Guide> guideFactory)
+        {
+            _guideService = guideService;
+            _agencyService = agencyService;
+            _guideFactory = guideFactory;
+        }
+        public IActionResult Index()
+        {
+            var guides = _guideService.GetAll();
+            return View(guides);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = _guideFactory.CreateInstance();
+            var agencyList = _agencyService.GetAll();
+            model.Agencies = agencyList.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Create(Guide model)
+        {
+            if (ModelState.IsValid)
+            {
+                _guideService.Create(model);
+                return RedirectToAction("Index", "Guide", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Update(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Error", "Home", new { area = "Admin" });
+            }
+            var agencyList = _agencyService.GetAll();
+            var model = _guideService.Get(id);
+            model.Agencies = agencyList.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Update(Guide model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.UpdatedAt = DateTime.Now;
+                _guideService.Update(model);
+                return RedirectToAction("Index", "Guide", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Error", "Home", new { area = "Admin" });
+            }
+            var agencyList = _agencyService.GetAll();
+            var model = _guideService.Get(id);
+            model.Agencies = agencyList.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Delete(Guide model)
+        {
+            if (model.Id == Guid.Empty)
+            {
+                return RedirectToAction("Error", "Home", new { area = "Admin" });
+            }
+            _guideService.Delete(model.Id);
+            return RedirectToAction("Index", "Guide", new { area = "Admin" });
+        }
     }
 }
