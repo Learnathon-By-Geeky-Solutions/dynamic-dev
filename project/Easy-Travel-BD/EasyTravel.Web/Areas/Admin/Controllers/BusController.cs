64cab2a442dbad3e1340d7c3e6246f78e8b3f977
abdurrashed <rashedroni1201@gmail.com>
2025-02-15 20:54:38 +0600

    added bus create in bus controller and IbusService inject in buscontroller

--- project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/BusController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/BusController.cs
@@ -30,68 +30,6 @@ namespace EasyTravel.Web.Areas.Admin.Controllers
             }
             return View();
         }
-        public IActionResult Index()
-        {
-            var buses = _busService.GetAllBuses();
-            return View(buses);
-        }
-
-
-
-        [HttpGet]
-        public IActionResult Update(Guid BusId)
-        {
-            var bus = _busService.GetBusById(BusId);
-            if (bus == null)
-            {
-                return NotFound();
-            }
-
-
-            return View(bus);
-
-        }
-
-
-        [HttpPost]
-        public IActionResult Update(Bus bus)
-        {
-            if (ModelState.IsValid)
-            {
-                _busService.UpdateBus(bus);
-                return RedirectToAction("Index", "Bus", new { area = "Admin" });
-            }
-            return View();
-
-        }
-
-
-        [HttpGet]
-        public IActionResult Delete(Guid BusId)
-        {
-            var bus = _busService.GetBusById(BusId);
-            if (bus == null)
-            {
-                return NotFound();
-            }
-
-
-            return View(bus);
-
-        }
-
-        [HttpPost]
-        public IActionResult Delete(Bus bus)
-        {
-            if (ModelState.IsValid)
-            {
-                _busService.DeleteBus(bus);
-                return RedirectToAction("Index", "Bus", new { area = "Admin" });
-            }
-            return View();
-
-        }
-
 
     }
 }
