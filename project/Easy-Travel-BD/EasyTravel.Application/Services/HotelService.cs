adc8f0b415caba462fd51103dd9356fb61146ccf
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2025-02-17 01:32:46 +0600

    Hotel related repository and service are added.

---
+++ project/Easy-Travel-BD/EasyTravel.Application/Services/HotelService.cs
@@ -0,0 +1,50 @@
+﻿using EasyTravel.Domain;
+using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Application.Services
+{
+    public class HotelService : IHotelService
+    {
+        private readonly IApplicationUnitOfWork _applicationunitOfWork;
+
+        public HotelService(IApplicationUnitOfWork applicationUnitOfWork)
+        {
+            _applicationunitOfWork = applicationUnitOfWork;
+        }
+
+
+
+        public void Create(Hotel entity)
+        {
+            _applicationunitOfWork.HotelRepository.Add(entity);
+            _applicationunitOfWork.Save();
+
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hotel Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hotel> GetAll()
+        {
+            return _applicationunitOfWork.HotelRepository.GetAll();
+        }
+
+        public void Update(Hotel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/HotelController.cs
@@ -0,0 +1,40 @@
+﻿using EasyTravel.Application.Services;
+using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTravel.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HotelController : Controller
+    {
+        private readonly IHotelService _hotelService;
+        public HotelController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        public IActionResult Index()
+        {
+            //var hotels = _hotelService.GetAll();
+            //return View(hotels);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Hotel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _hotelService.Create(model);
+                return RedirectToAction("Index", "Hotel", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
