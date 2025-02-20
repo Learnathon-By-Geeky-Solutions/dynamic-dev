c11e9b34459f7474d9cd79ffe722619733634b29
abdurrashed <rashedroni1201@gmail.com>
2025-02-20 22:12:00 +0600

    update area name in carcontroller , updated in create view, updated in indext view , added carcontroller name in adminsidepanel

--- project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/CarController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/CarController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EasyTravel.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CarController : Controller
     {
         private readonly ICarService _carService;
