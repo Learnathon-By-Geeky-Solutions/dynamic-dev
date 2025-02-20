4be1ec77f778fbbb7ccf406f57dc95b1dfda1f97
abdurrashed <rashedroni1201@gmail.com>
2025-02-20 21:37:14 +0600

    created carservice , added CarCreate method and Geallcars() , added defendency injection ICarRepository in ApplicationUnitofWork, added service and repository register in webmodule

---
+++ project/Easy-Travel-BD/EasyTravel.Application/Services/CarService.cs
@@ -0,0 +1,32 @@
+﻿using EasyTravel.Domain;
+using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Application.Services
+{
+    public class CarService : ICarService
+    {
+
+        private readonly IApplicationUnitOfWork _applicationUnitOfWork1;
+        public CarService(IApplicationUnitOfWork applicationUnitOfWork)
+        {
+            _applicationUnitOfWork1 = applicationUnitOfWork;
+
+        }
+        public void CreateCar(Car car)
+        {
+            _applicationUnitOfWork1.CarRepository.AddCar(car);
+            _applicationUnitOfWork1.Save();
+        }
+
+        public IEnumerable<Car> GetAllBuses()
+        {
+            return _applicationUnitOfWork1.CarRepository.GetAllCars();
+        }
+    }
+}
