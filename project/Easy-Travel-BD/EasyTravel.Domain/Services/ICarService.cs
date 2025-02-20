a7dc8d8cd4fe76e4eddd28c41ad3d9f09c02aab4
abdurrashed <rashedroni1201@gmail.com>
2025-02-20 21:05:29 +0600

    added IcarRepository class that implement method addcar and getallbusses , added Icarservice

---
+++ project/Easy-Travel-BD/EasyTravel.Domain/Services/ICarService.cs
@@ -0,0 +1,16 @@
+﻿using EasyTravel.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Domain.Services
+{
+    public interface ICarService
+    {
+
+        void CreateCar(Car car);
+        IEnumerable<Car> GetAllBuses();
+    }
+}
