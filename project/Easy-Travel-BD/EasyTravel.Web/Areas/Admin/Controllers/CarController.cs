a21c92bb7fd45924fc9b7ee73ee7b7dd9029fe81
abdurrashed <rashedroni1201@gmail.com>
2025-02-20 21:55:44 +0600

    added CarController , CarService injection in carcontroller , Crate view, index view, _carform

---
+++ project/Easy-Travel-BD/EasyTravel.Web/Areas/Admin/Controllers/CarController.cs
@@ -0,0 +1,36 @@
+﻿using EasyTravel.Application.Services;
+using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTravel.Web.Areas.Admin.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                _carService.CreateCar(car);
+                return RedirectToAction("Index", "Car", new { area = "Admin" });
+
+            }
+            return View();
+        }
+
+
+
+
+    }
+}
