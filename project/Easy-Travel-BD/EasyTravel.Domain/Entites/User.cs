bfea48c3ef635b1d6011f0fe77f7a9e2715236ca
IrfanMahmud <irfanmahmud201@gmail.com>
2025-02-26 13:30:32 +0600

    added models for login and register and modifies the views and action methods of them

--- project/Easy-Travel-BD/EasyTravel.Domain/Entites/User.cs
+++ project/Easy-Travel-BD/EasyTravel.Domain/Entites/User.cs
@@ -11,11 +11,11 @@ namespace EasyTravel.Domain.Entites
 {
     public class User : IdentityUser<Guid>
     {
-        public override Guid Id {  get; set; }
-        public required string? FirstName { get; set; }
-        public required string? LastName { get; set; }
-        public required string? Gender { get; set; }
-        public required DateTime? DateOfBirth { get; set; }
+        //public override Guid Id {  get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public DateTime? CreatedAt { get; set; }
         
