cc8941d2737aec76d729b6943ec792055c0a2675
IrfanMahmud <irfanmahmud201@gmail.com>
2025-03-02 00:37:35 +0600

    added and registered interface and implementaion for guide

---
+++ project/Easy-Travel-BD/EasyTravel.Application/Services/GuideService.cs
@@ -0,0 +1,29 @@
+﻿using EasyTravel.Domain;
+using EasyTravel.Domain.Entites;
+using EasyTravel.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Application.Services
+{
+    public class GuideService : IGuideService
+    {
+        private readonly IApplicationUnitOfWork _applicationUnitOfWork;
+        public GuideService(IApplicationUnitOfWork applicationUnitOfWork)
+        {
+            _applicationUnitOfWork = applicationUnitOfWork;
+        }
+        public Guide Get(Guid id)
+        {
+            return _applicationUnitOfWork.GuideRepository.GetById(id);
+        }
+
+        public IEnumerable<Guide> GetAll()
+        {
+            return _applicationUnitOfWork.GuideRepository.GetAll();
+        }
+    }
+}
