b330826eb39148456b1b7c697479c9243bf7a7a2
IrfanMahmud <irfanmahmud201@gmail.com>
2025-03-02 01:29:32 +0600

    added new style in Index.css and applied them in index viw of guide and pg and dependency added in guide controller

--- project/Easy-Travel-BD/EasyTravel.Web/Controllers/GuideController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Controllers/GuideController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyTravel.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyTravel.Web.Controllers
 {
     public class GuideController : Controller
     {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var models = _guideService.GetAll();
+            return View(models);
         }
     }
 }
