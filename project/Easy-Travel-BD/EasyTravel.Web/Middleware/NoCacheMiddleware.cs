8c3de3d1628f1b3e865dda06a9f9831495fdaea0
IrfanMahmud <irfanmahmud201@gmail.com>
2025-03-10 11:22:15 +0600

    NoCacheMiddleware and RequestLoggingMiddleware added for handling responses

---
+++ project/Easy-Travel-BD/EasyTravel.Web/Middleware/NoCacheMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace EasyTravel.Web.Middleware
+{
+    public class NoCacheMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public NoCacheMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            context.Response.Headers["Pragma"] = "no-cache";
+            context.Response.Headers["Expires"] = "0";
+            await _next(context);
+        }
+    }
+}
