ac986009dd3340365003a960ec986bab834444d5
abdurrashed <rashedroni1201@gmail.com>
2025-03-13 23:20:11 +0600

    updated carbookingmigration

--- project/Easy-Travel-BD/EasyTravel.Web/Migrations/20250313171023_CarbookingMigration.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Migrations/20250313171023_CarbookingMigration.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace EasyTravel.Web.Migrations
 {
     /// <inheritdoc />
-    public partial class CarBookingandCarMigrationsUpdate : Migration
+    public partial class CarbookingMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -43,16 +43,7 @@ namespace EasyTravel.Web.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[,]
-                {
-                    { new Guid("292dcaf2-aadc-493a-8f19-e7905ab98299"), null, "hotelManager", "hotelManager" },
-                    { new Guid("4558e034-03af-4d30-819f-9a24cb81c942"), null, "agencyManager", "agencyManager" },
-                    { new Guid("862b8016-7786-4cf2-bcb1-a4aac017ff2c"), null, "carManager", "carManager" },
-                    { new Guid("c10f83b0-9008-468b-b931-5e73ff416337"), null, "busManager", "busManager" }
-                });
+         
 
             migrationBuilder.CreateIndex(
                 name: "IX_CarBookings_CarId",
@@ -65,26 +56,6 @@ namespace EasyTravel.Web.Migrations
         {
             migrationBuilder.DropTable(
                 name: "CarBookings");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: new Guid("292dcaf2-aadc-493a-8f19-e7905ab98299"));
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: new Guid("4558e034-03af-4d30-819f-9a24cb81c942"));
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: new Guid("862b8016-7786-4cf2-bcb1-a4aac017ff2c"));
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: new Guid("c10f83b0-9008-468b-b931-5e73ff416337"));
 
             migrationBuilder.DropColumn(
                 name: "IsAvailable",
