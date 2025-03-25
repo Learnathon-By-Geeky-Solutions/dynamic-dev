bae03740d5d651ec8c4aae5218b02e7c5a2624f8
IrfanMahmud <irfanmahmud201@gmail.com>
2025-03-25 12:47:48 +0600

    added UserRole in admin dashboard

---
+++ project/Easy-Travel-BD/EasyTravel.Domain/Services/IAdminUserRoleService.cs
@@ -0,0 +1,18 @@
+﻿using EasyTravel.Domain.Entites;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Domain.Services
+{
+    public interface IAdminUserRoleService
+    {
+        Task<IdentityResult> CreateAsync(Guid UserId, Guid RoleId);
+        Task<IdentityResult> UpdateAsync(Guid UserId, Guid RoleId);
+        Task<IdentityResult> DeleteAsync(Guid UserId, Guid RoleId);
+        Task<IEnumerable<(User, Role)>> GetAllAsync();
+    }
+}
