4ca0132bb387d5041a31db913c3dbadbc2a48bcf
IrfanMahmud <irfanmahmud201@gmail.com>
2025-03-29 23:41:46 +0600

    modifies and implementation completed for user crud in admin area

--- project/Easy-Travel-BD/EasyTravel.Web/Models/RegisterViewModel .cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Models/RegisterViewModel .cs
@@ -4,11 +4,12 @@ namespace EasyTravel.Web.Models
 {
     public class RegisterViewModel
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Gender { get; set; }
-        public DateTime? DateOfBirth { get; set; }
+        public  string? FirstName { get; set; }
+        public  string? LastName { get; set; }
+        public  string? Email { get; set; }
+        public  string? Gender { get; set; }
+        public  DateTime? DateOfBirth { get; set; }
+        public  string? PhoneNumber { get; set; }
         public string Password { get; set; }
 
         [Compare("Password")]
