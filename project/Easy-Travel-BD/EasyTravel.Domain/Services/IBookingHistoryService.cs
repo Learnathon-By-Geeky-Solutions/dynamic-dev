9df8b45e1d8823d564ef74d120d1af8b56ea719c
IrfanMahmud <irfanmahmud201@gmail.com>
2025-04-08 09:32:38 +0600

    added and implemented service for booking history

---
+++ project/Easy-Travel-BD/EasyTravel.Domain/Services/IBookingHistoryService.cs
@@ -0,0 +1,18 @@
+﻿using EasyTravel.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravel.Domain.Services
+{
+    public interface IBookingHistoryService
+    {
+        Task<IEnumerable<BusBooking>> GetBusBookings(Guid id);
+        Task<IEnumerable<CarBooking>> GetCarBookings(Guid id);
+        Task<IEnumerable<HotelBooking>> GetHotelBookings(Guid id);
+        Task<IEnumerable<PhotographerBooking>> GetPhotographerBookings(Guid id);
+        Task<IEnumerable<GuideBooking>> GetGuideBookings(Guid id);
+    }
+}
