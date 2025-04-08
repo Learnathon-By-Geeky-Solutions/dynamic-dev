5404f37db2b53aca73a3a637fb060ce6260be2fa
abdurrashed <rashedroni1201@gmail.com>
2025-04-08 15:31:50 +0600

    busserarchformmodel  added

---
+++ project/Easy-Travel-BD/EasyTravel.Web/Models/BusSearchFormModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyTravel.Web.Models
+{
+    public class BusSearchFormModel
+    {
+     
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime dateTime { get; set; }
+
+    }
+}
