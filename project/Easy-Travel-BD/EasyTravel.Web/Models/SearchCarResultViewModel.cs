2aeafe97a5fafe2e631ac9e9f26077ca47c873a1
abdurrashed <rashedroni1201@gmail.com>
2025-04-09 16:01:25 +0600

    added CarSearchcontroller and implement this index action ,added SearchCarResultViewModel

---
+++ project/Easy-Travel-BD/EasyTravel.Web/Models/SearchCarResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EasyTravel.Domain.Entites;
+
+namespace EasyTravel.Web.Models
+{
+    public class SearchCarResultViewModel
+    {
+        public BusSearchFormModel CarSearchFormModel { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+
+
+    }
+}
