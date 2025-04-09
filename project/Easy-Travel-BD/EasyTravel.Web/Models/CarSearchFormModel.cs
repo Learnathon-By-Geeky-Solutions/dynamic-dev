ff67d72c8a05e38ce6930fb1d86019183fcc7876
abdurrashed <rashedroni1201@gmail.com>
2025-04-09 15:35:31 +0600

    added CarSearchFormModel

---
+++ project/Easy-Travel-BD/EasyTravel.Web/Models/CarSearchFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyTravel.Web.Models
+{
+    public class CarSearchFormModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime DepartureTime { get; set; }
+    }
+}
