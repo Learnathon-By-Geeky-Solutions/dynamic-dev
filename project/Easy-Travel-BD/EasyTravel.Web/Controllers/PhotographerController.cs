d888512707e2b2d1f2b8712997e101995f3b5dfe
IrfanMahmud <irfanmahmud201@gmail.com>
2025-04-17 07:41:39 +0600

    added booking repository , identityseeddata and modifications in pgbooking , guidebooking , photographer services and pg and booking controllers

--- project/Easy-Travel-BD/EasyTravel.Web/Controllers/PhotographerController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Controllers/PhotographerController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Build.Graph;
 using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
 using System.Net.WebSockets;
 using System.Security.Claims;
 using System.Text.Json;
@@ -20,15 +21,13 @@ namespace EasyTravel.Web.Controllers
         private readonly UserManager<User> _userManager;
         private readonly IMapper _mapper;
         private readonly ISessionService _sessionService;
-        private readonly IPhotographerBookingService _photographerBookingService;
-        public PhotographerController(IPhotographerService photographerService, UserManager<User> userManager, IMapper mapper, ISessionService sessionService, IAgencyService agencyService, IPhotographerBookingService photographerBookingService)
+        public PhotographerController(IPhotographerService photographerService, UserManager<User> userManager, IMapper mapper, ISessionService sessionService, IAgencyService agencyService)
         {
             _photographerService = photographerService;
             _userManager = userManager;
             _mapper = mapper;
             _sessionService = sessionService;
             _agencyService = agencyService;
-            _photographerBookingService = photographerBookingService;
         }
         [HttpGet]
         public IActionResult Index()
@@ -44,82 +43,15 @@ namespace EasyTravel.Web.Controllers
             {
                 SearchFormModel = new SearchFormModel
                 {
-                    EventDate = DateTime.Parse(_sessionService.GetString("EventDate")),
-                    StartTime = TimeSpan.Parse(_sessionService.GetString("StartTime")),
-                    TimeInHour = int.Parse(_sessionService.GetString("TimeInHour")),
-                    EndTime = TimeSpan.Parse(_sessionService.GetString("EndTime")),
+                    EventDate = DateTime.Parse(_sessionService.GetString("EventDate"), CultureInfo.InvariantCulture),
+                    StartTime = TimeSpan.Parse(_sessionService.GetString("StartTime"), CultureInfo.InvariantCulture),
+                    TimeInHour = int.Parse(_sessionService.GetString("TimeInHour"), CultureInfo.InvariantCulture),
+                    EndTime = TimeSpan.Parse(_sessionService.GetString("EndTime"), CultureInfo.InvariantCulture),
                 },
             };
             var pgBookingModel = _mapper.Map<PhotographerBooking>(model.SearchFormModel);
             model.Photographers = await _photographerService.GetPhotographerListAsync(pgBookingModel);
             return View(model);
-        }
-        [HttpGet]
-        public async Task<IActionResult> Book(Guid id)
-        {
-            var model = new PhotographerBooking()
-            {
-                EventDate = DateTime.Parse(_sessionService.GetString("EventDate")),
-                StartTime = TimeSpan.Parse(_sessionService.GetString("StartTime")),
-                TimeInHour = int.Parse(_sessionService.GetString("TimeInHour")),
-                EndTime = TimeSpan.Parse(_sessionService.GetString("EndTime")),
-                PhotographerId = id,
-            };
-
-            if (await _photographerBookingService.IsBooked(model) == true)
-            {
-                TempData["Message"] = "This photographer is already booked for this time slot. Please choose another time slot.";
-                return RedirectToAction("/Photographer/List");
-            }
-            _sessionService.SetString("PhotographerId", id.ToString());
-            if (User.Identity?.IsAuthenticated == false)
-            {
-                return RedirectToAction("Login", "Account", new {string.Empty});
-            }
-            return RedirectToAction("Review", "Photographer");
-        }
-
-        [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Book(PhotographerBookingViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var pgBooking = _mapper.Map<PhotographerBooking>(model);
-                if (User.Identity.IsAuthenticated == true)
-                {
-                    //pgBooking.UserId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
-                }
-                pgBooking.PhotographerId = Guid.Parse(_sessionService.GetString("PhotographerId"));
-                _sessionService.Remove("PhotographerId");
-                _photographerBookingService.AddBooking(pgBooking);
-                return View();
-            }
-            return View(model);
-        }
-        [HttpGet]
-        public async Task<IActionResult> Review()
-        {
-            var pgId = _sessionService.GetString("PhotographerId");
-            if (string.IsNullOrEmpty(pgId))
-            {
-                return Redirect("/Photographer/List");
-            }
-            _sessionService.SetString("LastVisitedPage", "/Photographer/Review");
-            var pgBooking = new PhotographerBookingViewModel();
-            var user = await _userManager.GetUserAsync(User);
-            pgBooking = _mapper.Map<PhotographerBookingViewModel>(user);
-            var photographer = _photographerService.Get(Guid.Parse(pgId));
-            pgBooking.Photographer = photographer;
-            var agency = _agencyService.Get(photographer.AgencyId);
-            pgBooking.AgencyName = agency.Name;
-            pgBooking.EventDate = DateTime.Parse(_sessionService.GetString("EventDate"));
-            pgBooking.StartTime = TimeSpan.Parse(_sessionService.GetString("StartTime"));
-            pgBooking.EndTime = TimeSpan.Parse(_sessionService.GetString("EndTime"));
-            pgBooking.TimeInHour = int.Parse(_sessionService.GetString("TimeInHour"));
-            pgBooking.EventType = _sessionService.GetString("EventType");
-            pgBooking.EventLocation = _sessionService.GetString("EventLocation");
-            pgBooking.TotalAmount = photographer.HourlyRate * pgBooking.TimeInHour;
-            return View(pgBooking);
         }
     }
 }
