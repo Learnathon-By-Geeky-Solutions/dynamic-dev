5592abc21fff1348d7f14f5f7f8bd72bed43cda2
IrfanMahmud <irfanmahmud201@gmail.com>
2025-04-24 15:38:24 +0600

    almost max issues are fixed except payment controller

--- project/Easy-Travel-BD/EasyTravel.Web/Controllers/CarController.cs
+++ project/Easy-Travel-BD/EasyTravel.Web/Controllers/CarController.cs
@@ -6,6 +6,7 @@ using EasyTravel.Web.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace EasyTravel.Web.Controllers
@@ -38,7 +39,7 @@ namespace EasyTravel.Web.Controllers
             // Retrieve search parameters from session
             var from = _sessionService.GetString("From");
             var to = _sessionService.GetString("To");
-            var dateTime = DateTime.Parse(_sessionService.GetString("DateTime"));
+            var dateTime = DateTime.Parse(_sessionService.GetString("DateTime"),CultureInfo.InvariantCulture);
 
             // Create the model to pass to the view
             var model = new SearchCarResultViewModel
@@ -59,6 +60,10 @@ namespace EasyTravel.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> PassengerDetails(Guid id)
         {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("List", "Car");
+            }
             var user = await _userManager.GetUserAsync(User);
             var car = _carService.GetCarById(id);
             if (car == null) return NotFound();
@@ -69,9 +74,9 @@ namespace EasyTravel.Web.Controllers
                 CarId = id,
                 BookingForm = new BookingForm
                 {
-                    PassengerName = $"{user.FirstName},{user.LastName}",
-                    Email = user.Email,
-                    PhoneNumber = user.PhoneNumber,
+                    PassengerName = $"{user?.FirstName},{user?.LastName}",
+                    Email = user?.Email!,
+                    PhoneNumber = user?.PhoneNumber!,
                     TotalAmount = car.Price
                 }
             };
